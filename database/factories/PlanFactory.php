7882650802dde5e8ea2c43ba7986625f6aa4968d
Lucas Vinicius <lucasvinicius211@yahoo.com>
2023-04-01 02:09:14 -0300

    refactor: Call name as method

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -12,7 +12,7 @@ class PlanFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->name(),
         ];
     }
 }
