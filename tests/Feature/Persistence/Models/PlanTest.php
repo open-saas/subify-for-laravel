cd290145196c98ce5884a6fdc850475d76009efa
Lucas Vinicius <lucasvinicius211@yahoo.com>
2023-04-01 01:46:56 -0300

    test: Add a test over regimes relationship

--- tests/Feature/Persistence/Models/PlanTest.php
+++ tests/Feature/Persistence/Models/PlanTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Persistence\Models;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use OpenSaaS\Subify\Persistence\Models\Plan;
+use OpenSaaS\Subify\Persistence\Models\PlanRegime;
 use Tests\Feature\TestCase;
 
 class PlanTest extends TestCase
@@ -34,5 +35,24 @@ class PlanTest extends TestCase
             'id' => $plan->id,
             'name' => $plan->name,
         ]);
+    }
+
+    public function test_it_has_regimes(): void
+    {
+        $plan = Plan::create([
+            'name' => 'Test Plan',
+        ]);
+
+        $planRegime = PlanRegime::factory()
+            ->for($plan)
+            ->create();
+
+        $this->assertDatabaseHas('plan_regimes', [
+            'id' => $planRegime->id,
+            'name' => $planRegime->name,
+            'plan_id' => $plan->id,
+        ]);
+
+        $this->assertEquals($plan->regimes->first()->id, $planRegime->id);
     }
 }
