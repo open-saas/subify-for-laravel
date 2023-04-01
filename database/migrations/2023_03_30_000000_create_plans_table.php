42540e073cc956ae935632d6f4fe347841daaece
Lucas Vinicius <lucasvinicius211@yahoo.com>
2023-04-01 01:17:57 -0300

    feat: Add soft deleting to Plan model

--- database/migrations/2023_03_30_000000_create_plans_table.php
+++ database/migrations/2023_03_30_000000_create_plans_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create(config('subify.persistence.eloquent.plan.table'), function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- src/Persistence/Models/Plan.php
+++ src/Persistence/Models/Plan.php
@@ -3,9 +3,12 @@
 namespace OpenSaaS\Subify\Persistence\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Plan extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
     ];
