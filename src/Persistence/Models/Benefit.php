4a3123d4da1610a25e790af5d3e6ae1613ac45fe
Lucas Vinicius <lucasvinicius211@yahoo.com>
2023-04-01 22:55:08 -0300

    feat: Cast unit to enum

--- src/Persistence/Models/Benefit.php
+++ src/Persistence/Models/Benefit.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OpenSaaS\Subify\Database\Factories\BenefitFactory;
+use OpenSaaS\Subify\Enums\PeriodicityUnit;
 
 class Benefit extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $casts = [
+        'periodicity_unit' => PeriodicityUnit::class,
+    ];
 
     protected $fillable = [
         'name',
